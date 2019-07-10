89cf460fbcac207bce85dc8497611ae8c095373c
dhubbard <dhubbard@oath.com>
2019-07-10 13:02:01 -0700

    Remove extra uneeded tests

--- tests/test__sphinx.py
+++ tests/test__sphinx.py
@@ -2,40 +2,10 @@ import inspect
 import os
 from unittest import TestCase, skip
 
-import markdown
-import sphinx_markdown_tables
 import sphinx_testing
 
 
-class AppMock(object):
-    def connect(self, dest, destfunc):
-        self.connect_dest = dest
-        self.connect_destfunc = destfunc
-
-
 class MarkdownTableTestCase(TestCase):
-
-    @skip
-    def test__setup(self):
-        app = AppMock()
-        result = sphinx_markdown_tables.setup(app)
-        self.assertEqual(app.connect_dest, 'source-read')
-        self.assertEqual(app.connect_destfunc, sphinx_markdown_tables.process_tables)
-        self.assertIn('version', result.keys())
-
-    @skip
-    def test__process_tables__without_table(self):
-        app = AppMock()
-        source = ['# Test']
-        sphinx_markdown_tables.process_tables(app, 'index', source)
-        self.assertEqual('# Test', source[0])
-
-    @skip
-    def test__process_tables__with_table(self):
-        app = AppMock()
-        source = ['# Test\n\n| Header1 | Header2 |\n| ------- | ------- |\n| 1       | 2       |\n']
-        sphinx_markdown_tables.process_tables(app, 'index', source)
-        self.assertIn('<table', source[0])
 
     def test__render_table__without_sphinx_markdown_tables_enabled(self):
         """
