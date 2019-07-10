7aace32a0bca7214d7d64b35c8f4a57efef87a7c
dhubbard <dhubbard@oath.com>
2019-07-10 13:29:07 -0700

    Add pylint configuration to run pylint locally from tox
    Pylint fixes
    
    Add .pylintrc to have pylint ignore unused arguments for functions that are called by sphinx that expect to pass those arguments.

--- sphinx_markdown_tables/__init__.py
+++ sphinx_markdown_tables/__init__.py
@@ -1,3 +1,4 @@
+"""sphinx markdown table extension module"""
 import re
 
 import markdown
@@ -9,19 +10,21 @@ __version__: str = pkg_resources.get_distribution("dhubbard-sphinx-markdown-tabl
 
 
 def setup(app):
+    """Sphinx extension setup for sphinx-markdown-table extention"""
     app.connect('source-read', process_tables)
     return {'version': __version__}
 
 
 def process_tables(app, docname, source):
     """
-    Convert markdown tables to html, since recommonmark can't. This requires 3 steps:
+    Convert markdown tables to html, since recommonmark can't.
+    This requires 3 steps:
         Snip out table sections from the markdown
         Convert them to html
         Replace the old markdown table with an html table
 
-    This function is called by sphinx for each document. `source` is a 1-item list. To update the document, replace
-    element 0 in `source`.
+    This function is called by sphinx for each document. `source` is a 1-item list.
+    To update the document, replace element 0 in `source`.
     """
     md = markdown.Markdown(extensions=['markdown.extensions.tables'])
     table_processor = markdown.extensions.tables.TableProcessor(md.parser)
@@ -32,7 +35,9 @@ def process_tables(app, docname, source):
     for i, block in enumerate(blocks):
         if table_processor.test(None, block):
             html = md.convert(block)
-            styled = html.replace('<table>', '<table border="1" class="docutils">', 1)  # apply styling
+
+            # apply styling
+            styled = html.replace('<table>', '<table border="1" class="docutils">', 1)
             blocks[i] = styled
 
     # re-assemble into markdown-with-tables-replaced
