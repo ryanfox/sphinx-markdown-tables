e79a7b8294ff9445c9fe760f1f02824425e82d65
joao.macedo <magau.macedo@gmail.com>
2020-01-28 17:05:38 +0100

    Render .rst and .md file names, linked inside the table block...

--- sphinx_markdown_tables/__init__.py
+++ sphinx_markdown_tables/__init__.py
@@ -28,10 +28,25 @@ def process_tables(app, docname, source):
 
     for i, block in enumerate(blocks):
         if table_processor.test(None, block):
-            html = md.convert(block)
+            html = md.convert(
+                render_file_links(block)
+            )
             styled = html.replace('<table>', '<table border="1" class="docutils">', 1)  # apply styling
             blocks[i] = styled
 
     # re-assemble into markdown-with-tables-replaced
     # must replace element 0 for changes to persist
     source[0] = '\n\n'.join(blocks)
+    
+def render_file_links(block, source_suffix=['.md','.rst']):
+    """Render file links inside the table block,
+    replacing the file extension in the source_suffix list by '.html'.
+    """
+    link_pattern = re.compile(
+        "\] *\( *(?P<link>.*)({}) *\)".format(
+            "|".join(source_suffix).replace('.','\.')))
+    rendered_block, n = re.subn(link_pattern, "](\g<link>.html)", block)
+    while n > 0:
+        rendered_block, n = re.subn(link_pattern, "](\g<link>.html)",
+                                    rendered_block)
+    return rendered_block
